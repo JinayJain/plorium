93dd8296e46d86262e5b2749488ad3ae71d424aa
Jinay Jain <jinaybjain@gmail.com>
2022-05-05 12:34:47 -0400

    Install prettier for code formatting

--- src/components/roadmap/Block.tsx
+++ src/components/roadmap/Block.tsx
@@ -1,18 +1,3 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
-
-const Block = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => (
-  <Box bg="green.50" shadow="md" px={4} py={6} rounded="lg">
-    <Heading size="md" color="green.800" mb={1}>
-      {title}
-    </Heading>
-    <Text>{description}</Text>
-  </Box>
-);
+interface BlockProps {}
 
 export default Block;
