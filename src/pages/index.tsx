a2f091e9bad75a5067a673b1d8577aa6f0b3ade0
Jinay Jain <jinaybjain@gmail.com>
2022-09-05 00:56:50 -0400

    Update landing page with mission and email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Box h="100vh" bg="gray.100">
-      <Flex h="100%" align="center" justify="center">
-        <Heading as="h1" size="2xl" color="gray.700">
-          Under Construction
-        </Heading>
+    <Box bg="gray.100" h="100vh">
+      <Flex h="100%" w="100%" align="center" justify="center">
+        <Stack maxW="40%" spacing={4}>
+          <Heading>Plorium, the exploration emporium</Heading>
+          <Text color="gray.600" fontSize="lg">
+            Plorium aims to drastically simplify the self-learning process. We
+            want to allow individuals to learn what they want in the most
+            efficient and enjoyable way possible.
+          </Text>
+          <Text color="gray.600">
+            Are you an avid self-learner? Email us at{" "}
+            <Link href="mailto:hello@plorium.com" textDecor="underline">
+              hello@plorium.com
+            </Link>
+          </Text>
+        </Stack>
       </Flex>
     </Box>
   );
