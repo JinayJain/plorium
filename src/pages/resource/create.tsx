fa9b6ab5b66788947511ec1882d144a3afea24ca
Jinay Jain <jinaybjain@gmail.com>
2022-09-06 09:32:53 -0400

    Add validation and toast to resource creator form

--- src/pages/resource/create.tsx
+++ src/pages/resource/create.tsx
@@ -2,18 +2,20 @@ import { SmallAddIcon } from "@chakra-ui/icons";
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormHelperText,
   FormLabel,
   Heading,
   Input,
   Select,
   Stack,
-  Text,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ResourceType } from "@prisma/client";
-import { useFormik } from "formik";
-import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import Layout from "@/components/layout/Layout";
 import { trpc } from "@/util/trpc";
@@ -24,77 +26,75 @@ const ResourceTypeOptions = Object.keys(ResourceType).map((key) => ({
   label: key[0] + key.slice(1).toLowerCase(),
 }));
 
+const createResourceSchema = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().min(1),
+  type: z.nativeEnum(ResourceType),
+  url: z.string().url(),
+});
+
+type CreateResourceValues = z.infer<typeof createResourceSchema>;
+
 function CreateResource() {
   const createResourceMutation = trpc.useMutation("resource.create");
-  const router = useRouter();
+  const toast = useToast();
 
-  const { values, handleSubmit, handleChange } = useFormik({
-    initialValues: {
-      name: "",
-      description: "",
-      url: "",
-      type: ResourceTypeOptions[0].value,
-    },
-    onSubmit: async ({ name, description, url, type }) => {
-      try {
-        const result = await createResourceMutation.mutateAsync({
-          name,
-          description,
-          url,
-          type: type as ResourceType,
-        });
-
-        router.push(`/resource/${result.id}`);
-      } catch (error) {
-        console.log(error);
-      }
-    },
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateResourceValues>({
+    resolver: zodResolver(createResourceSchema),
+    mode: "onBlur",
   });
+
+  const onSubmit = async (values: CreateResourceValues) => {
+    await createResourceMutation.mutateAsync(values);
+
+    toast({
+      title: "Resource created.",
+      status: "success",
+      isClosable: true,
+    });
+
+    reset();
+  };
 
   return (
     <Layout>
       <Heading>Create Resource</Heading>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <Stack spacing={4}>
-          <FormControl>
+          <FormControl isInvalid={!!errors.name} isRequired>
             <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              name="name"
-              onChange={handleChange}
-              value={values.name}
-            />
+            <Input type="text" {...register("name")} />
+            <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl>
+          <FormControl isInvalid={!!errors.url} isRequired>
             <FormLabel>URL</FormLabel>
-            <Input
-              type="url"
-              name="url"
-              onChange={handleChange}
-              value={values.url}
-            />
+            <Input type="url" {...register("url")} />
             <FormHelperText>https://example.com/resource</FormHelperText>
+            <FormErrorMessage>{errors.url?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl>
+          <FormControl isInvalid={!!errors.description} isRequired>
             <FormLabel>Description</FormLabel>
-            <Textarea
-              name="description"
-              onChange={handleChange}
-              value={values.description}
-            />
+            <Textarea {...register("description")} />
             <FormHelperText>
               Share more about this resource and why it&apos;s great
             </FormHelperText>
+            <FormErrorMessage>{errors.description?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl>
+          <FormControl isInvalid={!!errors.type} isRequired>
             <FormLabel>Resource Type</FormLabel>
-            <Select name="type" onChange={handleChange} value={values.type}>
+            <Select {...register("type")}>
               {ResourceTypeOptions.map(({ value, label }) => (
                 <option key={value} value={value}>
                   {label}
                 </option>
               ))}
             </Select>
+            <FormErrorMessage>{errors.type?.message}</FormErrorMessage>
           </FormControl>
 
           <Button
